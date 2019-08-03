263b56a3b2a5523fb55a0553cad7f433309a4bee
Nikhilesh <nikhileshxiii@gmail.com>
2019-08-03 17:36:56 -0500

    Zuul config

--- ZuulService/src/main/java/com/example/ZuulService/ZuulServiceApplication.java
+++ ZuulService/src/main/java/com/example/ZuulService/ZuulServiceApplication.java
@@ -1,9 +1,14 @@
 package com.example.ZuulService;
 
+import com.example.ZuulService.filter.ErrorFilter;
+import com.example.ZuulService.filter.PostFilter;
+import com.example.ZuulService.filter.PreFilter;
+import com.example.ZuulService.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +17,22 @@ public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 }
--- ZuulService/src/main/java/com/example/ZuulService/config/SecurityTokenConfig.java
+++ ZuulService/src/main/java/com/example/ZuulService/config/SecurityTokenConfig.java
@@ -4,33 +4,37 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.ZuulService.security.JwtTokenAuthenticationFilter;
 
-@EnableWebSecurity
-public class SecurityTokenConfig extends WebSecurityConfiguration {
-
-    @Autowired
-    private JwtConfig jwtConfig;
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().exceptionHandling().authenticationEntryPoint((req, rsp, e)-> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                .and().addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                	.antMatchers(org.springframework.http.HttpMethod.POST, jwtConfig.getUri()).permitAll()
-                	.antMatchers("/gallery"+"/admin/**").hasRole("ADMIN")
-                	.anyRequest().authenticated();
-    }
-    
-    @Bean
-    public JwtConfig jwtConfig() {
-    	return new JwtConfig();
-    }
-}
+//@EnableWebSecurity
+//public class SecurityTokenConfig extends WebSecurityConfiguration {
+//
+//    @Autowired
+//    private JwtConfig jwtConfig;
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().exceptionHandling().authenticationEntryPoint((req, rsp, e)-> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+//                .and().addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+//                .authorizeRequests()
+//                	.antMatchers(org.springframework.http.HttpMethod.POST, jwtConfig.getUri()).permitAll()
+//                	.antMatchers("/gallery"+"/admin/**").hasRole("ADMIN")
+//                	.anyRequest().authenticated();
+//    }
+//
+//    @Bean
+//    public JwtConfig jwtConfig() {
+//    	return new JwtConfig();
+//    }
+//}
