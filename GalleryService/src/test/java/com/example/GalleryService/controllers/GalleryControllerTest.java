ab08ba3e5591a5e40a5a2481dbed9503daee73b1
Nikhilesh <nikhileshxiii@gmail.com>
2019-09-15 16:55:46 -0500

    Add tests in GalleryService

--- GalleryService/src/test/java/com/example/GalleryService/controllers/GalleryControllerTest.java
+++ GalleryService/src/test/java/com/example/GalleryService/controllers/GalleryControllerTest.java
@@ -1,51 +1,56 @@
 package com.example.GalleryService.controllers;
 
+import com.example.GalleryService.GalleryServiceApplication;
 import com.example.GalleryService.entities.Gallery;
 import com.example.GalleryService.service.GalleryService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = {GalleryServiceApplication.class})
 @AutoConfigureMockMvc
 public class GalleryControllerTest {
-    @Autowired
-    private RestTemplate restTemplate;
 
-//    @Autowired
-//    private GalleryController galleryController;
-
-    @Autowired
     private MockMvc mockMvc;
 
-//    @Autowired
-    @Mock
+    @MockBean
     private GalleryService galleryService;
 
-    @Value("${local.server.port")
-    private String port;
+//    @Value("${local.server.port")
+//    private String port;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void home() throws Exception {
-        this.mockMvc.perform(get("/"))
+        this.mockMvc.perform(get("/galleries/home"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content()
-                        .string("Hello from Gallery Service running at port: " + port)
+                        .string("Hello from Gallery Service")
                 );
     }
 
@@ -55,9 +60,11 @@ public class GalleryControllerTest {
         gallery.setId(2);
         gallery.setGalleryName("testgallery");
         when(galleryService.saveGallery(gallery)).thenReturn(gallery);
-        this.mockMvc.perform(get("/2")).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect((ResultMatcher) gallery);
+        this.mockMvc.perform(get("/galleries/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//                .andExpect((ResultMatcher) gallery);
     }
 
     @Test
@@ -66,10 +73,10 @@ public class GalleryControllerTest {
 
     @Test
     public void homeAdmin() throws Exception {
-        this.mockMvc.perform(get("/admin"))
+        this.mockMvc.perform(get("/galleries/admin"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content()
-                        .string("This is the admin area of Gallery service running at port: " + port)
+                        .string("This is the admin area of Gallery service")
                 );
     }
 }
