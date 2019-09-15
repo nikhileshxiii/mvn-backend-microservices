487cc17f9b49398c4a987e03cdd4c461b521de1f
Nikhilesh <nikhileshxiii@gmail.com>
2019-09-15 16:46:51 -0500

    Add eureka-server ApplicationTest file

--- eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,16 +1,40 @@
 package com.example.eurekaserver;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaServerApplicationTests {
 
+    private MockMvc mockMVC;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        this.mockMVC = webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testEurekaPath() throws Exception {
+        this.mockMVC.perform(get("/")).andDo(print()).andExpect(status().isOk());
     }
 
 }
